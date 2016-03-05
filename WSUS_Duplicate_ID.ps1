255aed31261b89f5418b46e82e5cc9b10b546a1a
bcprescott <bcprescott@outlook.com>
2016-03-04 21:57:51 -0600

    WSUS script to generate a new SUS ID
    Run this on the individual machine...

---
+++ WSUS_Duplicate_ID.ps1
@@ -0,0 +1,9 @@
+﻿#Removes duplicate Client ID from server this is ran on, and re-authorizes with WSUS. 
+net stop wuauserv
+net stop bits
+REG DELETE "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate" /v AccountDomainSid /f
+REG DELETE "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate" /v PingID /f
+REG DELETE "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate" /v SusClientId /f
+REG DELETE "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate" /v SusClientIdValidation /f
+net start wuauserv
+wuauclt /resetauthorization /detectnow
